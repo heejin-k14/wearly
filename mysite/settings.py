258765d30411f61551613bd8382f055e30b6f06e
HJ <mbu5533@naver.com>
2020-08-27 19:30:13 +0900

    heroku edited

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'wearly_real',
+    'HOST': 'localhost',
+    'PORT': '3306',
+    'USER': 'root',
+    'PASSWORD': '1234'
+  }
 }
 
 
